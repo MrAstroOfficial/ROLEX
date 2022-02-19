2264375f507657ecbefed482d97d42bda31c07b5
Mr-A S T R O <96073683+MrAstroOfficial@users.noreply.github.com>
2022-02-19 15:52:29 +0530

    Update R-SOURCE.lua

--- R-SOURCE.lua
+++ R-SOURCE.lua
@@ -1,5 +1,5 @@
-local script32bit = 'c-oWpDNPJ!ketlRCB=/U!otB)5ypT38s!d+2p%I7Gmz8*Vh#p_1}rBT29P8RdvR){S!xl8%=K6(6U/.OENno6jlqGz)Sd:Xt-+oe+Z}Iy6$b[.kZu-]0;0eT.@M@gRF{-eocweVyAN_GOakH-Af5:sQ)/fcT=HW348$E!B?IB[2+lk]CCv)oEL@!I.fY0gnR*3fthq]4-bk+.0RW-]J/k'
-local script64bit = 'c-oWpDNPJ!ketlRCB=/U!NS2(5ypT38s!d+2p%I7Gmz8*Vh#p_1}rBT29P8RdvR){S!xl8%=K6(6U/.OENno6jlqGz)Sd:Xt-+oe+Z}Iy6$b[.kZu-]0;0eT.@M@gRF{-eocweVyAN_GOakH-Af5:sQ)/fcT=HW348$E!B?IB[2+lk]CCv)oEL@!I.fY0gnR*3fthq]4-bk+.0RW{aJ/_'
+local script32bit = 'c-oWpDNPJ!ketlRCB=/U!otB)5ypT38s!d+2p%H}ACP2YaA06{aA0t3U_;+jU_=iB0Aw;TGPF53G8{7g1c?:h*f]_1t5IWUVBi4iWns:QX5mP!NKGy,NoA2PNi5FJ(dkYC)05bN((+ZGOD*OG()j0Sv,)7n78fU]r?CY(:H#b}+nOBgVG/Cx0P#d1o)f_t:qdoIW,3i;Dro?XMj-BnDrr[wWZ{v!2}RA[%gjq;Va-kQ[ZBfkw;mi@HL*Z$FFi9aRky.zEZ*pSH#peUMAtDa!U+IbP%t0]1rz]PM2%Y:'
+local script64bit = 'c-oWpDNPJ!ketlRCB=/U!NS2(5ypT38s!d+2p%H}ACP2YaA06{aA0t3U_;+jU_=iB0Aw;TGPF53G8{7g1c?:h*f]_1t5IWUVBi4iWns:QX5mP!NKGy,NoA2PNi5FJ(dkYC)05bN((+ZGOD*OG()j0Sv,)7n78fU]r?CY(:H#b}+nOBgVG/Cx0P#d1o)f_t:qdoIW,3i;Dro?XMj-BnDrr[wWZ{v!2}RA[%gjq;Va-kQ[ZBfkw;mi@HL*Z$FFi9aRky.zEZ*pSH#peUMAtDa!U+IbP%t0]1rz]PNl^xN'
 
 if   cheatEngineIs64Bit() then
      decodeFunction(script64bit)()
