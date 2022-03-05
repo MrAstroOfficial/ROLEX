4eb8d946617b400aa72722fb005e472cc07c0ae4
Mr-A S T R O <96073683+MrAstroOfficial@users.noreply.github.com>
2022-03-05 20:48:24 +0530

    Update SERVER - 0.lua

--- SERVER - 0.lua
+++ SERVER - 0.lua
@@ -1,7 +1,3 @@
-local script32bit = 'c-oWpDNPJ!ketlRCB=/U!otB)5ypT38s!d+2p%I7E0ELyVh#p_1}vu/1^H={1}rBT29P2awjh5W+9Z]k5i9@E+ugqkd7*f42M30fh6aZAh64--fczyuy}ht6V0Z#F:oo[_SnCs?+b{J1APYqTGD:DcimmkZixSKA)lbjkN_Q;9Q.U-G@GZ[lO7xQRbM/_T9E)ee@8M4(GLthCbM)PZ)a%lH%,n8c4GMD(QZ,Kr1!6/JoYF*Vp7hibPoU/aywnmFb_5D-$cKd@B@6^uB?1g#eqsuew^ZXf2Vy;I'
-local script64bit = 'c-oWpDNPJ!ketlRCB=/U!NS2(5ypT38s!d+2p%I7E0ELyVh#p_1}vu/1^H={1}rBT29P2awjh5W+9Z]k5i9@E+ugqkd7*f42M30fh6aZAh64--fczyuy}ht6V0Z#F:oo[_SnCs?+b{J1APYqTGD:DcimmkZixSKA)lbjkN_Q;9Q.U-G@GZ[lO7xQRbM/_T9E)ee@8M4(GLthCbM)PZ)a%lH%,n8c4GMD(QZ,Kr1!6/JoYF*Vp7hibPoU/aywnmFb_5D-$cKd@B@6^uB?1g#eqsuew^ZXf3;H3a'
-
-if   cheatEngineIs64Bit() then
-     decodeFunction(script64bit)()
-else decodeFunction(script32bit)()
+function testing()
+messageDialog("server working!", mtInformation, mbOK)
 end
